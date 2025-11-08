313d8b0539827b3fe227c78648883a79e98534ee
Daniel Xu <64868985+x0ba@users.noreply.github.com>
2025-11-08 11:49:51 -0800

    switch to scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import "./App.css";
+import "./App.scss";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <NavBar />
       <Home />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import { Container, Row } from "react-bootstrap";
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <Container fluid>
+          <Row className="bg-primary text-white">Footer Section</Row>
+        </Container>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
